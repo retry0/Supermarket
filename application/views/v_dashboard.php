6fdc302e9dfbe9ff74e1cfccb9b637e8532f0eaf
Pratama Adi <31929388+retry0@users.noreply.github.com>
2017-09-25 12:26:09 +0800

    Update v_dashboard.php

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -8,17 +8,6 @@
 							<br>
 							<br>
 							<align ="center">AUTHOR</align>
-							<br>
-							<br>
-							<br>
-							<font color="red"><marquee> LINGGA ADI PRATAMA 3311501039 <br></marquee></font>
-							<br>
-							<br>
-							<br>
-							<br>
-							<br>
-							<br>
-							<font color="orange"><marquee> Welly Andriani 3311501054 <br></marquee></font>
 							</body>
                         </h2>
                     </div>
